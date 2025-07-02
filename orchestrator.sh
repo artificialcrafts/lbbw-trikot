2da39594f40ec993f754eab173b21ad61b70ff07
Markus Frank <markus.frank@jvm.com>
2025-07-02 13:15:49 +0200

    Enhance orchestrator script to run Docker containers with GPU support; add new workflow templates for image processing and background removal

--- orchestrator.sh
+++ orchestrator.sh
@@ -15,11 +15,9 @@ for i in $(seq 1 $NUM_WORKERS)
 do
   # Use -d to run the container in "detached" (background) mode
   # Give each container a unique name for easy identification
-  docker run -d --rm \
-    --name "lbbw-worker-$i" \
-    -e REPLICATE_API_TOKEN \
-    -e SQS_QUEUE_URL="https://sqs.eu-central-1.amazonaws.com/320819923469/lbbw-trikot-queue" \
-    "$IMAGE_NAME"
+  docker run --gpus all -it --rm \
+  -e REPLICATE_API_TOKEN \
+  lbbw-trikot-sqs:latest
 done
 
 echo "All workers started. Use 'docker logs -f lbbw-worker-1' to view logs for a specific worker."
