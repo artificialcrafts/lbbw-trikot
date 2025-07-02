6caca1403bd6c5d80834f397cf8bfd77b35b61f0
Markus Frank <markus.frank@jvm.com>
2025-07-02 13:16:25 +0200

    Fix orchestrator script to run Docker containers in detached mode

--- orchestrator.sh
+++ orchestrator.sh
@@ -15,7 +15,7 @@ for i in $(seq 1 $NUM_WORKERS)
 do
   # Use -d to run the container in "detached" (background) mode
   # Give each container a unique name for easy identification
-  docker run --gpus all -it --rm \
+  docker run --gpus all -it --rm -d \
   -e REPLICATE_API_TOKEN \
   lbbw-trikot-sqs:latest
 done
