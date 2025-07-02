c10a34cb2304aa719d4648132891f6df0db5f2e9
Markus Frank <markus.frank@jvm.com>
2025-07-02 16:47:32 +0200

    Set number of worker containers to 1 in orchestrator.sh

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NUM_WORKERS=5
+NUM_WORKERS=1
 IMAGE_NAME="lbbw-trikot-sqs:latest"
 
 # Check for the REPLICATE_API_TOKEN
@@ -16,8 +16,8 @@ do
   # Use -d to run the container in "detached" (background) mode
   # Give each container a unique name for easy identification
   docker run --gpus all -it --rm -d --memory=2g --memory-swap=4g --name lbbw-worker-$i \
-  -e REPLICATE_API_TOKEN \
-  lbbw-trikot-sqs:latest
+    -e REPLICATE_API_TOKEN \
+    lbbw-trikot-sqs:latest
 done
 
 echo "All workers started. Use 'docker logs -f lbbw-worker-1' to view logs for a specific worker."
