7d599ffb9b3bd3e8731d8614c7a3f730d0ebaff4
Markus Frank <markus.frank@jvm.com>
2025-07-03 14:19:26 +0200

    Fix formatting in runbaby.sh by adding a newline at the end of the file

--- runbaby.sh
+++ runbaby.sh
@@ -2,5 +2,5 @@
 UUID=$(uuidgen)
 docker run --gpus all -it --rm \
   -e REPLICATE_API_TOKEN \
-  -e WARMUP_JOB_PATH=s3://lbbw-trikot/workflow-assets/33429fd4-7ba1-4680-a7a3-44c895bf45cd.json
+  -e WARMUP_JOB_PATH=s3://lbbw-trikot/workflow-assets/33429fd4-7ba1-4680-a7a3-44c895bf45cd.json \
   lbbw-trikot-sqs:latest
