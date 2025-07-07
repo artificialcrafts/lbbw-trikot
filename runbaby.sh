4fcc8732bcff820e5eafae8e4af66497f2df71c7
Markus Frank <markus.frank@jvm.com>
2025-07-07 10:33:59 +0200

    Update WARMUP_JOB_PATH in runbaby.sh to point to the correct asset JSON file

--- runbaby.sh
+++ runbaby.sh
@@ -2,5 +2,5 @@
 UUID=$(uuidgen)
 docker run --gpus all -it --rm \
   -e REPLICATE_API_TOKEN \
-  -e WARMUP_JOB_PATH=s3://lbbw-trikot/workflow-assets/d1a83069-ac7c-47a0-ae1a-083265d5a98e.json \
+  -e WARMUP_JOB_PATH=s3://lbbw-trikot/workflow-assets/d5dfdea5-b466-495d-86dc-3767b7b29c3e.json \
   lbbw-trikot-sqs:latest
