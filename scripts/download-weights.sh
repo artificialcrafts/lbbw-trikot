7e4a5f8888fef6cdc2b4eea733d536aeabd74fb1
Markus Frank <markus.frank@jvm.com>
2025-07-07 11:02:57 +0200

    Refactor download-weights.sh to streamline model download process and create necessary directories

--- scripts/download-weights.sh
+++ scripts/download-weights.sh
@@ -3,12 +3,6 @@ set -e
 
 echo "--- Pre-warming model caches ---"
 
-# Create target directories
-mkdir -p /app/ComfyUI/models/BiRefNet
-mkdir -p /root/.cache/torch/hub/checkpoints/
-mkdir -p /app/ComfyUI/models/LLM
-mkdir -p /app/ComfyUI/models/RMBG/RMBG-2.0
-
 # Download SAM2 Hiera Base Plus Model
 echo "Downloading sam2_hiera_base_plus.safetensors..."
 mkdir -p /app/ComfyUI/models/sam2
@@ -16,10 +10,28 @@ pget -xf "https://weights.replicate.delivery/default/comfy-ui/sam2/sam2_hiera_ba
 
 # Download crest.safetensors
 # echo "Downloading crest.safetensors..."
+mkdir -p /app/ComfyUI/models/loras
 aws s3 cp s3://lbbw-trikot/workflow-assets/crest.safetensors /app/CComfyUI/models/loras
+
+# Download clip_1.safetensors
+echo "Downloading clip_1.safetensors..."
+mkdir -p /app/ComfyUI/models/text_encoders
+pget https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors /app/ComfyUI/models/text_encoders
+
+# Download vae.safetensors
+echo "Downloading vae.safetensors..."
+mkdir -p /app/ComfyUI/models/vae
+pget https://huggingface.co/lovis93/testllm/resolve/ed9cf1af7465cebca4649157f118e331cf2a084f/ae.safetensors /app/ComfyUI/models/text_encoders
+
+# Download inswapper_128.safetensors
+echo "Downloading inswapper_128.safetensors..."
+mkdir -p /app/ComfyUI/models/insightface
+pget https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx /app/ComfyUI/models/text_encoders
+
 
 # Download BiRefNet Models
 echo "Downloading BiRefNet models..."
+mkdir -p /app/ComfyUI/models/BiRefNet
 pget -xf "https://weights.replicate.delivery/default/comfy-ui/BiRefNet/swin_large_patch4_window12_384_22kto1k.pth.tar" /app/ComfyUI/models/BiRefNet/
 pget -xf "https://weights.replicate.delivery/default/comfy-ui/BiRefNet/pvt_v2_b2.pth.tar" /app/ComfyUI/models/BiRefNet/
 pget -xf "https://weights.replicate.delivery/default/comfy-ui/BiRefNet/BiRefNet-ep480.pth.tar" /app/ComfyUI/models/BiRefNet/
@@ -29,6 +41,7 @@ pget -xf "https://weights.replicate.delivery/default/comfy-ui/BiRefNet/pvt_v2_b5
 
 # Download ControlNet Aux Model
 echo "Downloading ControlNet Aux models..."
+mkdir -p /root/.cache/torch/hub/checkpoints/
 pget -xf "https://weights.replicate.delivery/default/comfy-ui/custom_nodes/comfyui_controlnet_aux/mobilenet_v2-b0353104.pth.tar" /root/.cache/torch/hub/checkpoints/
 
 # Download Florence-2-base (LLM) Model
